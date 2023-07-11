bd3b017c4e50528a2fd8cd9b9888be857bbc428a
fapraxmarer <pf0690@mci4me.at>
2023-07-11 20:31:12 +0200

    Changes to base player movements

--- Assets/Scripts/PlayerMovements/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovements/PlayerMovement.cs
@@ -5,32 +5,55 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
+    //player base variables
     private CharacterController controller;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
-    public float speedMultiplier = 4.0f;
-    public float playerSpeed = 4.0f;
-    private float jumpHeight = 1.0f;
-    public float rotationSpeed = 190;
-    public float gravityValue = -9.81f;
+
+    //character Speed values
+    public float baseSpeed = 5.0f;
+    public float speedMultiplier = 3.0f;
+    private float playerSpeed;
+
+    //rotation variables
+    public float baseRotation = 190;
+    private float rotationSpeed;
+    public float rotationMultiplier = 1.3f;
+
+    //jump variables
+    public float jumpHeight = 1.5f;
     private int jumpCounter = 0;
+    public int maxJumps = 2;
+
+    //physic variables
+    public float gravityMultiplier = 3.2f;
+    private float gravityValue;
+
 
     private void Start()
     {
+        //initiate gravity, need the multiplier, cant be used before because the compiler hasnt initiated it yet
+        gravityValue = -9.81f * gravityMultiplier;
         controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
+       
+        //initiated grounded
         groundedPlayer = controller.isGrounded;
 
-        if (groundedPlayer)
+        //reset values for base state
+        if (jumpCounter == 0)
         {
-            jumpCounter = 0;
+            playerSpeed = baseSpeed;
+            rotationSpeed = baseRotation;
         }
-
+       
+        //resets grounded values
         if (groundedPlayer && playerVelocity.y < 0)
         {
+            jumpCounter = 0;
             playerVelocity.y = 0f;
         }
 
@@ -38,34 +61,30 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        // Rotate character
-        transform.Rotate(0f, horizontalInput * rotationSpeed * Time.deltaTime, 0f);
-
+        //makes character sprint
         if(Input.GetKey(KeyCode.LeftShift) && jumpCounter == 0)
         {
-            Debug.Log("test");
             playerSpeed += speedMultiplier;
+            rotationSpeed = baseRotation / rotationMultiplier;
         }
+
+        // Rotate character
+        transform.Rotate(0f, horizontalInput * rotationSpeed * Time.deltaTime, 0f);
 
         // Move character forward
         Vector3 move = transform.forward * verticalInput;
         controller.Move(move * Time.deltaTime * playerSpeed);
 
         // Jump logic
-        if (Input.GetKeyDown(KeyCode.Space) && jumpCounter < 3)
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCounter < maxJumps)
         {
             playerVelocity.y = 0f;
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
             jumpCounter += 1;
         }
 
-        // Apply gravity
+        // Apply gravity and then move character in local space
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
-
-        if (jumpCounter == 0)
-        {
-            playerSpeed = speedMultiplier;
-        }
     }
 }
